cb2c1602d532e024f668150e2b915e853903361c
trevjnels <trevorjohnnels@gmail.com>
2019-05-22 11:52:51 -0600

    on the surfice this thing visually done. This is a huge turning point and prehaps a moment to submit a pull req. I do need to fix the few things listed in my last commit note: DB/seeder throws a tantrum ever 3-15 seeds, there are a handful of other dynamic things I could render, also need to add functionality to the buttons and fix the drop down. First things, first: get the db/seed bug out.

--- src/itemDeets.jsx
+++ src/itemDeets.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 export  const Deets = (props) =>  (
-    <div className=" tjn-flex tjn-flex-col tjn-deets">
-    <div className="tjn-align-start tjn-mini-header">Item details</div>
-      <div><p>{props.description[0]}</p></div>
-      <div><p>{props.description[1]}</p></div>
-      <div><p>{props.description[2]}</p></div>
-      <div><p>{props.description[3]}</p></div>
-      <div><p>{props.description[4]}</p></div>
-      <div><p>{props.description[5]}</p></div>
-      <div><p>{props.description[6]}</p></div>
+    <div  className=" tjn-flex tjn-flex-col tjn-deets">
+    <div   className="tjn-align-start tjn-mini-header">Item details</div>
+      <div className="tjn-deet-p tjn-deet-p1"  >{props.description[0]}</div>
+      <div className="tjn-deet-p tjn-deet-p2" >{props.description[1]}</div>
+      <div className="tjn-deet-p tjn-deet-p3" >{props.description[2]}</div>
+      <div className="tjn-deet-p tjn-deet-p4" >{props.description[3]}</div>
+      <div className="tjn-deet-p tjn-deet-p5" >{props.description[4]}</div>
+      <div className="tjn-deet-p tjn-deet-p6" >{props.description[5]}</div>
+      <div className="tjn-deet-p tjn-deet-p7" >{props.description[6]}</div>
     </div>
   )
 
--- src/shippingAndReturns.jsx
+++ src/shippingAndReturns.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 export const Shipping =(props) => (
     <div className="tjn-shipping tjn-flex tjn-flex-col tjn-flex-center tjn-align-start">
       <div className="tjn-align-start tjn-mini-header"> Shipping & Returns</div>
-      <div className="tjn-mini-header2"> Get it soon. Estimated delivery: {props.eta[0].replace("-", " ")}-{props.eta[1].slice(-2)} </div>
-      <div> Need it sooner? Upgrade shipping in the cart</div>
-      <div> From {props.location}</div>
-      <div> {props.shipprice}  to United States, 84103-1506</div>
-      <div className="tjn-mini-header2">No returns or exchanges</div>
+      <div className="tjn-shippingInfo">
+          <div className="tjn-mini-header2"> Get it soon. Estimated delivery: {props.eta[0].replace("-", " ")}-{props.eta[1].slice(-2)} </div>
+          <div className="tjn-needItSoon">Need it sooner? Upgrade shipping in checkout!</div>
+          <div className="tjn-toFromShipping"> 
+            <div className="tjn-from">From {props.location.split(", ")[1]}
+            </div>
+            <div className="tjn-to"> {props.shipprice}  to <u>United States, 84103-1506</u></div>
+            </div>
+          </div>
+          <div className="tjn-mini-header2">No returns or exchanges</div>
     </div>
   )
 
