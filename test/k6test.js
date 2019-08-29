3784c212257d8a4898b5b286715c0b5bfde7eca5
Abhishek Mallepalli <mallepalliabhishek@gmail.com>
2019-08-29 14:05:20 -0700

    Update k6test.js

--- test/k6test.js
+++ test/k6test.js
@@ -10,8 +10,6 @@ export default function () {
   var index = Math.floor(Math.random() * 10000000)
   let res = http.get("http://54.183.254.92:3010/items/" + index);
   check(res, {
-    // "request went through": (r) => r.status == 200,
-    //   "transaction time OK": (r) => r.timings.duration < 200,
 
   })
   sleep(1);
